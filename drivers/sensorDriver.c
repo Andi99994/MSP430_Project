d00d3f1b01826f8454a74033172892552c6000cf
Andreas <aer9932@students.fhv.at>
2018-02-05 17:57:24 +0100

    some code cleanup

--- drivers/sensorDriver.c
+++ drivers/sensorDriver.c
@@ -1,7 +1,21 @@
 #include "sensorDriver.h"
 
-uint8_t cnt = 0;
+uint8_t byteIndex = 0;
 uint8_t gTemperature[3];
+
+typedef const uint8_t DeviceAddress_t;
+
+typedef struct i2c_data {
+    const void *tx_buf;
+    size_t tx_len;
+    void *rx_buf;
+    size_t rx_len;
+} I2CData_t;
+
+static int sensorDriver_checkI2CAck(void);
+static int sensorDriver_receiveI2C(DeviceAddress_t deviceAddress, uint8_t *buf, size_t nbytes);
+static int sensorDriver_transmitI2C(DeviceAddress_t deviceAdress, const uint8_t *buf, size_t nbytes);
+static int sensorDriver_transferI2C(DeviceAddress_t deviceAddress, I2CData_t *data);
 
 void sensorDriver_initI2C(void) {
     P1SEL0 |= I2C_SDA_PIN + I2C_SCL_PIN;      // route i2c pins
@@ -11,83 +25,34 @@ void sensorDriver_initI2C(void) {
     UCB0CTLW1 |= UCASTP_2;                    // generate stop condition if byte counter UCB0TBCNT reached
     UCB0BRW = 4;                              // fSCL = SMCLK/4 = 250kHz
     UCB0TBCNT = 0x03;                         // generate stop condition after 3 bytes
-    UCB0I2CSA = 0x40;                         // slave Address (temp+humidity sensor SHT21)
+    UCB0I2CSA = TEMPERATURE_SENSOR_ADDRESS;   // slave Address (temp+humidity sensor SHT21)
     UCB0CTLW0 &= ~UCSWRST;                    // clear SW reset (i2c module resumes operation)
     UCB0IE |= UCRXIE | UCNACKIE | UCBCNTIFG;  // enable relevant interrupts
 }
 
-int sensorDriver_resetTemperatureSensor(void) {
-    int err;
-    static const uint8_t deviceAddress = 0x40;
-    struct i2c_data data;
-    uint8_t write_cmd[1];
-
-    write_cmd[0] = 0xFE;
-
-    data.tx_buf = &write_cmd;
-    data.tx_len = 1;
-    data.rx_buf = NULL;
-    data.rx_len = 0;
-
-    err = sensorDriver_transferI2C(deviceAddress, &data);
-    UCB0CTLW0 |= UCTXSTP;
-
-    return err;
-}
-
-int sensorDriver_readTemperatureValue(uint8_t rx_data[]) {
-    int err;
-    uint8_t deviceAddress = 0x40;
-    struct i2c_data data;
-
-    data.tx_buf = NULL;
-    data.tx_len = 0;
-    data.rx_buf = (uint8_t *) rx_data;
-    data.rx_len = 3;
-
-    err = sensorDriver_transferI2C(deviceAddress, &data);
-
-    return err;
-}
-
 int sensorDriver_measureTemperature(void) {
-    int err;
-    static const uint8_t deviceAddress = 0x40;
-    struct i2c_data data;
-    uint8_t write_cmd[1];
-    write_cmd[0] = 0xF3;
+    uint8_t write_cmd[1] = {0xF3};
+    I2CData_t data = { .tx_buf = &write_cmd, .tx_len = 1, .rx_buf = 0, .rx_len = 0 };
 
-    data.tx_buf = &write_cmd;
-    data.tx_len = 1;
-    data.rx_buf = NULL;
-    data.rx_len = 0;
-
-    err = sensorDriver_transferI2C(deviceAddress, &data);
+    int err = sensorDriver_transferI2C(TEMPERATURE_SENSOR_ADDRESS, &data);
     UCB0CTLW0 &= ~UCTR;
     UCB0CTLW0 |= UCTXSTT;
     return err;
 }
 
-//TODO is deviceAddress needed here?
-int sensorDriver_checkI2CAck(const uint8_t deviceAddress) {
+int sensorDriver_checkI2CAck(void) {
     int err = 0;
 
-    /* Check for ACK */
-    if (UCB0IFG & UCNACKIFG) {
-        /* Stop the I2C transmission */
-        UCB0CTLW0 |= UCTXSTP;
-
-        /* Clear the interrupt flag */
-        UCB0IFG &= ~UCNACKIFG;
-
-        /* Set the error code */
-        err = -1;
+    if (UCB0IFG & UCNACKIFG) {        /* Check for ACK */
+        UCB0CTLW0 |= UCTXSTP;         /* Stop the I2C transmission */
+        UCB0IFG &= ~UCNACKIFG;        /* Clear the interrupt flag */
+        err = -1;                     /* Set the error code */
     }
 
     return err;
 }
 
-int sensorDriver_receiveI2C(const uint8_t deviceAddress, uint8_t *buf, size_t nbytes) {
+int sensorDriver_receiveI2C(DeviceAddress_t deviceAddress, uint8_t *buf, size_t nbytes) {
     int err = 0;
 
     /* Send the start and wait */
@@ -106,7 +71,7 @@ int sensorDriver_receiveI2C(const uint8_t deviceAddress, uint8_t *buf, size_t nb
     }
 
     /* Check for ACK */
-    err = sensorDriver_checkI2CAck(deviceAddress);
+    err = sensorDriver_checkI2CAck();
 
     /* If no error and bytes left to receive, receive the data */
     while ((err == 0) && (nbytes > 0)) {
@@ -133,7 +98,7 @@ int sensorDriver_receiveI2C(const uint8_t deviceAddress, uint8_t *buf, size_t nb
     return err;
 }
 
-int sensorDriver_transmitI2C(const uint8_t deviceAddress, const uint8_t *buf, size_t nbytes) {
+int sensorDriver_transmitI2C(DeviceAddress_t deviceAddress, const uint8_t *buf, size_t nbytes) {
     int err = 0;
 
     /* Send the start condition */
@@ -143,17 +108,14 @@ int sensorDriver_transmitI2C(const uint8_t deviceAddress, const uint8_t *buf, si
     while ( (UCB0CTLW0 & UCTXSTT) && ( (UCB0IFG & UCTXIFG0) == 0) );
 
     /* Check for ACK */
-    err = sensorDriver_checkI2CAck(deviceAddress);
+    err = sensorDriver_checkI2CAck();
 
     /* If no error and bytes left to send, transmit the data */
     while ((err == 0) && (nbytes > 0)) {
         UCB0TXBUF = *buf;
         while ((UCB0IFG & UCTXIFG0) == 0) {
 
-            //UCB0CTLW0 |= UCTR | UCTXSTT;
-            //UCB0TXBUF = *buf;
-
-            err = sensorDriver_checkI2CAck(deviceAddress);
+            err = sensorDriver_checkI2CAck();
             if (err < 0) {
                 break;
             }
@@ -166,7 +128,7 @@ int sensorDriver_transmitI2C(const uint8_t deviceAddress, const uint8_t *buf, si
     return err;
 }
 
-int sensorDriver_transferI2C(const uint8_t deviceAddress, struct i2c_data *data) {
+int sensorDriver_transferI2C(DeviceAddress_t deviceAddress, I2CData_t *data) {
     int err = 0;
 
     /* Set the slave device address */
@@ -180,9 +142,6 @@ int sensorDriver_transferI2C(const uint8_t deviceAddress, struct i2c_data *data)
     /* Receive data if there is any */
     if ((err == 0) && (data->rx_len > 0)) {
         err = sensorDriver_receiveI2C(deviceAddress, (uint8_t *) data->rx_buf, data->rx_len);
-    } else {
-        /* No bytes to receive send the stop condition */
-       //UCB0CTLW0 |= UCTXSTP;
     }
 
     return err;
@@ -210,20 +169,18 @@ __interrupt void USCI_B0_ISR(void)
     case USCI_I2C_UCTXIFG1:  break;         // Vector 20: TXIFG1
 
     case USCI_I2C_UCRXIFG0:                 // Vector 22: RXIFG0
-       if(UCB0I2CSA == 0x40){
-           gTemperature[cnt] = UCB0RXBUF;                   // Get RX data
+       if(UCB0I2CSA == TEMPERATURE_SENSOR_ADDRESS){
+           gTemperature[byteIndex] = UCB0RXBUF;                   // Get RX data
        }
-       cnt++;
+       byteIndex++;
 
-       if(cnt == 3){
-           cnt = 0;
+       if(byteIndex == 3){
+           byteIndex = 0;
        }
       break;
 
     case USCI_I2C_UCTXIFG0:  break;         // Vector 24: TXIFG0
-    case USCI_I2C_UCBCNTIFG:
-        //lockI2C = 0;
-        break;         // Vector 26: BCNTIFG
+    case USCI_I2C_UCBCNTIFG: break;         // Vector 26: BCNTIFG
     case USCI_I2C_UCCLTOIFG: break;         // Vector 28: clock low timeout
     case USCI_I2C_UCBIT9IFG: break;         // Vector 30: 9th bit
     default: break;
--- drivers/sensorDriver.h
+++ drivers/sensorDriver.h
@@ -12,25 +12,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define I2C_SDA_PIN     (1 << 6)
-#define I2C_SCL_PIN     (1 << 7)
-#define NULL 0
-
-struct i2c_data {
-    const void *tx_buf;
-    size_t tx_len;
-    void *rx_buf;
-    size_t rx_len;
-};
+#define I2C_SDA_PIN                     (1 << 6)
+#define I2C_SCL_PIN                     (1 << 7)
+#define TEMPERATURE_SENSOR_ADDRESS      0x40
 
 void sensorDriver_initI2C(void);
 int sensorDriver_measureTemperature(void);
-int sensorDriver_checkI2CAck(const uint8_t deviceAddress);
-int sensorDriver_receiveI2C(const uint8_t deviceAddress, uint8_t *buf, size_t nbytes);
-int sensorDriver_transmitI2C(const uint8_t deviceAdress, const uint8_t *buf, size_t nbytes);
-int sensorDriver_transferI2C(const uint8_t deviceAddress, struct i2c_data *data);
-int sensorDriver_readTemperatureValue(uint8_t rx_data[]);
-int sensorDriver_resetTemperatureSensor(void);
-
 
 #endif /* DRIVERS_SENSORDRIVER_H_ */
